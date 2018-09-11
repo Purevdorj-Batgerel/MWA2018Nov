b29158e051ad209e6ca44cee3eb42468f8d8feb9
Purevdorj <purvee.hq@gmail.com>
2018-09-11 18:11:37 -0500

    moved files

--- src/LoginWindowController.java
+++
@@ -1,9 +0,0 @@
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-
-public class LoginWindowController {
-    @FXML
-    protected void handleSubmitButtonAction(ActionEvent event) {
-        MainWindow.changeScene("Main");
-    }
-}
--- src/MainWindowController.java
+++
@@ -1,9 +0,0 @@
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-
-public class MainWindowController {
-    @FXML
-    protected void handleSubmitButtonAction(ActionEvent event) {
-        MainWindow.changeScene("Login");
-    }
-}
