a6e2779e0ceb2214349137be715ad59dbab33710
Purevdorj <purvee.hq@gmail.com>
2018-09-12 15:33:35 -0500

    [Add] login window username password field

--- src/Controllers/LoginWindowController.java
+++ src/Controllers/LoginWindowController.java
@@ -3,17 +3,27 @@ package Controllers;
 import Main.MainWindow;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 
 public class LoginWindowController {
     @FXML
+    private TextField username;
+    @FXML
+    private PasswordField password;
+
+    @FXML
     public void handleSubmitButtonAction(ActionEvent event) {
-        if(checkValid()) {
+        if (checkValid()) {
             MainWindow.changeScene("Main");
         }
 
     }
 
     private boolean checkValid() {
+//        username.getText()
+//        password.getText()
         return true;
     }
 }
